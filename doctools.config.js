8b85118ba89256d9500339f581d797e2beb555f6
jms <jm@audio.d3ck.net>
2018-05-14 11:44:57 +0200

    WiP

--- doctools.config.js
+++ doctools.config.js
@@ -1,47 +1,89 @@
 const uikitConf = require('/Users/jms/uikit/doctools.config.js');
-const util = require('yootheme-doctools/src/util');
+
+const util = require('yootheme-doctools/src/util.js');
 const ModuleMapper = require('yootheme-doctools/src/plugins/ModuleMapper.js');
 const RuntimeAnalyzer = require('yootheme-doctools/src/plugins/RuntimeAnalyzer.js');
+const ComponentExporter = require('yootheme-doctools/src/plugins/ComponentExporter.js');
+
 const path = require('path');
 const _ = require('lodash');
+const {swap} = require('./utils');
+
+const introMapping = swap(require('./intro.json').introduction.items);
 
 module.exports = {
 
     ...uikitConf,
 
-    include: [ '/Users/jms/uikit/src/js/@(core|mixin|components)/*', '/Users/jms/uikit/docs/**/*.md'],
+    include: [ 'src/js/@(core|mixin|components)/*', 'docs/**/*.md', 'package.json', 'README.md'],
 
     menu: {
         ...require('./intro.json'),
         components : {
             path: 'components',
-            items: package => _.filter(package.getResources(), res => {
+            items: pack => _.filter(pack.getResources(), res => {
                 return util.match([
                     'src/js/core/*.js',
                     'src/js/components/*.js',
                     'src/js/mixins/*.js',
                     'docs/components/*.md'
-                ], res.path, {data:res, matchBase: '/Users/jms/uikit'}) && !res.isAsset;
+                ], res.path, {data:res, matchBase: uikitConf.base }) && !res.isAsset;
             })
 
         }
-        // utils: {
-        //     match: ['src/js/util/*.js']
-        // }
     },
+
+    plugins: [
+        ...uikitConf.plugins,
+        {
+            onGet(app, data) {
+
+                data.routeMap = _.reduce(app.resources, (map, res) => {
+
+                    if (res.path.includes('/intro/')) {
+                        // map[res.resource] = res.name;
+                        map[res.name] = res.resource;//res.resource;
+                    } else if(res.type === 'UIkitComponent' || (!res.isAsset && res.path.includes('/components/') )) {
+                        // map[res.resource] = 'component/' + res.name;//res.resource;
+                        map['component/' + res.name] = res.resource;//res.resource;
+                    }
+                    return map;
+                }, {});
+
+            }
+        },
+        new ComponentExporter({
+            output: __dirname + '/html',
+              createLink(app, desc, data) {
+                const map = swap(data.routeMap);
+                return map[desc.resource] || '#';
+              },
+              getFileName(app, desc, data) {
+                const map = swap(data.routeMap);
+                const name = map[desc.resource] + '.vue';
+                return name;
+              },
+              resources (app, data) {
+                return _.mapValues(data.routeMap, res => app.resources[res]);
+              },
+              postProcess(app, html) {
+                  return `<template>
+                    ${html}
+                  </template>`;
+              }
+        })
+    ],
 
     dev: true,
 
     output: {
         split: false,
-        // path: __dirname + '/docs'
         path: __dirname + '/docs.json'
     },
 
-    // getResourceName: desc => {
-    //     const name = desc.path.split('/');
-    //     name.pop();
-
-    //     return name.pop() + '-' + desc.name;
+    // getResourceUri(desc) {
+    //     return desc.fileInPackage.substr(1).replace(/\//g, '.').substr(1) || desc.name;
     // }
+
+
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,94 +1,51 @@
-// const {DoctoolsWebpack} = require('yootheme-doctools');
-
-const fs = require('fs');
-const _ = require('lodash');
-
-//generate a list of all components
-const allComponents = fs.readdirSync('/Users/jms/uikit/docs/components')
-                .filter(name => ~name.indexOf('.md'))
-                .map(name => name.replace('.md', ''))
-                .reduce((prev, curr) => {
-                  const interlacedCamel = curr.replace(/(-[a-z])/, res => " " + res[1].toUpperCase());
-                  const interlacesPascal = interlacedCamel.substr(0, 1).toUpperCase() + interlacedCamel.substr(1);
-                  prev[curr] = interlacesPascal;  //fs.readFileSync(`/Users/jms/uikit/docs/components/${curr}.md`, 'utf8');
-                  return prev;
-                }, {});
-
-                // debugger
-fs.writeFileSync('components.json', JSON.stringify(allComponents, null, 2));
-
-const sidebar = require('./docs.json').menu;
-
-const routes = _.reduce(sidebar, (prev, val) => {
-    return prev.concat(Object.values(val.items).map(route => '/documentation/' + (val.path && (val.path + '/' + route) || route )));
-}, []);
-
-
 module.exports = {
 
-  build: {
+	build: {
     extend(config, context) {
 
-      if (context.isClient) {
+			if (context.isClient) {
 
-        config.externals = {
-          'uikit': 'UIkit'
-        }
+				config.externals = {
+					'uikit': 'UIkit'
+				}
 
-        if (!context.isDev) {
-          config.externals = (context, request, callback) => {
-            // console.log(request);
-            if(
-            [/yootheme-doctools.*(vue|lodash.js|prismjs).*/].some(matcher => matcher.exec(request)) ||
-              ['uikit-icons', 'docs.json', 'highlight.js', 'marked'].some(name => request.includes(name)) ||
-              ['uikit'].some(name => name === request)) {
-                console.log('skip dep:', request);
-              return callback(null, 'null');
-            }
+			}
 
-            callback()
+		},
 
-          }
-        }
-      }
+		extractCSS: true,
 
-    },
-    extractCSS: true,
+		analyze: true,
 
-    analyze: true,
+	},
 
-    vendor: [
-      'yootheme-doctools',
-      // 'vue'
-    ]
+	plugins: [
 
-  },
+		'~/plugins/boot'
 
-  plugins: [
-    '~/plugins/boot'
-  ],
+	],
 
-  // render: {
-  //   static:
-  // },
+	modules: [
 
-  generate: {
-    routes,
-    // routes: ['/documentation/components/accordion', '/documentation/introduction']
-    // dir: '/Applications/MAMP/htdocs/dist',
+		'~/modules/doctools'
 
-  },
+	],
 
-  head: {
-      script: [
-        { src: '/uikit.min.js', type: 'text/javascript', charset: 'utf-8'},
-        { src: '/uikit-icons.min.js', type: 'text/javascript', charset: 'utf-8'}
-      ]
-    },
+	head: {
 
-  css: [
-    '~/less/theme.less'
-  ]
+		script: [
 
+			{ src: '/uikit.min.js', type: 'text/javascript', charset: 'utf-8' },
+			{ src: '/uikit-icons.min.js', type: 'text/javascript', charset: 'utf-8' }
+
+		]
+
+	},
+
+	css: [
+
+		'~/less/theme.less'
+
+	]
 
 }
